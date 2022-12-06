592d08bada5bfbf6dd6bd0685436a031948deb9d
Raphael Rebel <raphaelrebel@live.com>
2022-12-06 22:02:33 +0100

    Opdracht4: opdracht 1 af

---
+++ W1O4/arrays.php
@@ -0,0 +1,52 @@
+<?php
+
+$aanhef = array(
+    "aanhef" => "Beste",
+    "persoon" => "");
+    var_dump($aanhef);
+    echo('<br>');
+
+$korting = array(
+    "product" => "U heeft in de voorgaande bestelling voor [[product]] gekozen",
+    "korting" => "Bij Uw volgende bestelling kunnen we U een korting van 10% aanbieden op hetzelfde product"
+);
+var_dump($korting);
+echo('<br>');
+
+$ondertekening = array(
+  "groet" => "Met vriendelijke groet,",
+  "naam" => "",
+  "functie" => "Directeur Webshop beheer"
+);
+
+print_r($ondertekening);
+echo('<br>');
+
+/*
+ * Opdracht 1:
+ * Laat de inhoud van alle arrays zien
+ * Gebruik daarvoor de functies var_dump() en print_r()
+ */
+
+/*
+ * Opdracht 2:
+ * Vervang de waarde van persoon, in de array aanhef naar 'klant'
+ * Vervang de waarde van naam, in de array ondertekening naar 'jouw naam'
+ */
+
+/*
+ * Opdracht 3:
+ * Vervang het woord [[product]] door jouw gekozen product in de winkelmand opdracht
+ * Gebruik hiervoor de methode str_replace().
+ */
+
+/*
+ * Opdracht 4:
+ * Voeg nu alle arrays samen in een nieuwe array die je $aanbieding noemt.
+ * Gebruik hiervoor de methode array_merge()
+ */
+
+/*
+ * Opdracht 5:
+ * Toon de inhoud van de nieuwe array $aanbieding
+ */
